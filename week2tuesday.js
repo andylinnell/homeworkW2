0eb90980eff7c2feda0fdad5f02cc65d7bb716a9
Andy Linnell <andylinnell.cafo@gmail.com>
2023-01-17 19:31:29 -0500

    Week 2 Tuesday's homework.

---
+++ week2tuesday.js
@@ -0,0 +1,16 @@
+// 1. Write a function that takes an integer minutes and converts it to seconds.
+const minToSec = minutes => {
+    let seconds = minutes * 60;
+    console.log(`${minutes} minutes is ${seconds} seconds.`);
+}
+minToSec(5);
+
+// 2. Write a function that converts hours into seconds.
+const hourToSec = hours => {
+    let seconds = hours * 60**2;
+    console.log(`${hours} hours is ${seconds} seconds.`);
+}
+hourToSec(22);
+
+
+// 3.Create an SQL with your favorite dishes and cuisines. 
